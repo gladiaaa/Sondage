252d9e5b1cea3ee099c4fabc232307f32da39262
gladiaaa <118833423+gladiaaa@users.noreply.github.com>
2025-01-02 13:51:24 +0100

    Update setup_db.py

--- setup_db.py
+++ setup_db.py
@@ -24,16 +24,15 @@ collections = {
     ]
 }
 
-#collections et insertion des données
+
 for collection_name, data in collections.items():
     collection = db[collection_name]
     if collection_name == "users":
-        # Ajout de données uniques pour les utilisateurs
         for user in data:
             if not collection.find_one({"email": user["email"]}):
                 collection.insert_one(user)
     else:
-        # Ajout des scrutins
+
         collection.insert_many(data)
 
 print(f"Base de données '{db_name}' et collections créées avec succès !")
